32722a62112b7d0fb688e17e3609813e8ae23ddb
Paul Garrod <paulrgarrod@gmail.com>
2021-06-01 20:36:13 +1200

    update scss

--- src/Main.js
+++ src/Main.js
@@ -3,9 +3,8 @@ import './Main.css';
 function Main() {
   return (
     <main className="Main">
-        <h1>Content Header</h1>
         <section>
-        <h2>Section Header</h2>
+          <h2>Section Header</h2>
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
         </section>
