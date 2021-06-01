b441a1fa805f9df796f67e80e26ce810a4f875dc
Paul Garrod <paulrgarrod@gmail.com>
2021-06-01 20:25:19 +1200

    update scss

--- src/Main.js
+++ src/Main.js
@@ -3,9 +3,17 @@ import './Main.css';
 function Main() {
   return (
     <main className="Main">
-        <h1>Content</h1>
-        <p>Content section - testing font
-        </p>
+        <h1>Content Header</h1>
+        <section>
+        <h2>Section Header</h2>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+        </section>
+        <section>
+        <h2>Section Header</h2>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+        </section>
     </main>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,29 @@
 import './Sidebar.css';
 
 function Sidebar() {
+  const links = [
+    "link 1",
+    "link 2",
+    "link 3",
+    "link 4",
+    "link 5",
+    "link 6",
+    "link 7",
+    "link 8",
+  ];
+
+  const linkItems = links.map((link) => 
+  <li>{link}</li>
+  );
+
   return (
     <aside className="Sidebar">
-        <h1>Sidebar</h1>
+        <h1>Sidebar Header</h1>
+        <div className="Sidebar-list">
+          <ul>
+          {linkItems}
+          </ul>
+        </div>
     </aside>
   );
 }
